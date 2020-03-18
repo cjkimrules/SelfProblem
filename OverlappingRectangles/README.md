00d54879a12b830a3b520d59151ac4e0439a680c
CJ Kim <cjkimrules@gmail.com>
2020-03-18 13:33:30 -0500

    done

---
+++ OverlappingRectangles/README.md
@@ -0,0 +1,3 @@
+## Find the Area of Two Overlapping Rectangles
+
+This was an interview question asked by Amazon (posted online, I didn't had this question personally).
---
+++ OverlappingRectangles/main.py
@@ -0,0 +1,58 @@
+
+# Author: CJ Kim
+# Date: 3/18/2020
+# Title: Find the Area of Overlapping Rectangles
+# Explain: Input: [(0,4), (4,0)], [(4,8), (8,4)]
+#          Output: 1
+# Note: The coordinates are written in normal XY plane grid.
+#       The order is [(TopLeftX, TopLeftY), (BottomRightX, BottomRightY)]
+#
+
+class Rect:
+    TopLeftX = 0
+    TopLeftY = 0
+    BotRightX = 0
+    BotRightY = 0
+
+    def __init__(self, topX, topY, botX, botY):
+        self.TopLeftX = topX
+        self.TopLeftY = topY
+        self.BotRightX = botX
+        self.BotRightY = botY
+
+
+def FindOverlappingArea(Rect1, Rect2):
+    # Compare 2 leftmost x coords, find the biggest of those two.
+    LeftX = max(Rect1.TopLeftX, Rect2.TopLeftX)
+
+    # Find the smallest of rightmost x coords.
+    RightX = min(Rect1.BotRightX, Rect2.BotRightX)
+
+    # Do the same for Y coords. Compare topmost and find smallest,
+    # compare bottom-most and find the biggest.
+    TopY = min(Rect1.TopLeftY, Rect2.TopLeftY)
+    BottomY = max(Rect1.BotRightY, Rect2.BotRightY)
+    
+    Width = RightX - LeftX
+    Height = TopY - BottomY
+
+    # If either Width or Height is negative, we know there was no overlapping.
+    if Width <= 0 or Height <= 0:
+        return 0
+    else:
+        return Width * Height
+
+def Test(TestNum, Memo, Rect1, Rect2, output):
+    if(FindOverlappingArea(Rect1, Rect2) == output):
+        print("#" + str(TestNum) + " " + Memo + "\tPassed")
+    else:
+        print("#" + str(TestNum) + " " + Memo + "\tFailed")
+
+def main():
+    Test(1, "Simple Overlap",Rect(0,5,5,0), Rect(4,8,8,4), 1)
+    Test(2, "No Overlap", Rect(0,5,5,0), Rect(5,8,8,5), 0)
+    Test(3, "Identical Rects", Rect(0,5,5,0), Rect(0,5,5,0), 25)
+
+
+if __name__ == "__main__":
+    main()
